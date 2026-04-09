d08c9405f7282cc4334751e67ce593be9c3b282d
Lucas Cardozo <lucasivancardozo27@gmail.com>
2026-04-09 17:37:56 -0300

    fix: update site URL and canonical links to remove 'www' prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.pagensmedia.com.ar',
+  site: 'https://pagensmedia.com.ar',
   output: 'static',
   build: {
     format: 'directory',
